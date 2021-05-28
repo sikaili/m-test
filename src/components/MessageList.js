c988956bfd4eaeca82d98f9c81c384c56951ad5f
sikai li <skyl@me.com>
2021-05-29 00:18:21 +0200

    added:optimistic updating messageList, patch failded, first working version

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -2,10 +2,10 @@ import "../scss/MessageList.scss";
 
 import React, { useEffect, useState } from "react";
 import useInfiniteScroll from "react-infinite-scroll-hook";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { selectCurrentRealtor } from "../features/realtorSlice";
+import { selectCurrentRealtor, setCurrentRealtor } from "../features/realtorSlice";
 import { useFetch } from "../js/hooks/useFetch";
 import { usePrevious } from "../js/hooks/usePrevious";
 import Loader from "./Loader";
@@ -15,14 +15,15 @@ export default function MessageList() {
   const [url, setUrl] = useState("");
   const [pageNo, setPageNo] = useState(1);
   const [messageList, setMessageList] = useState([]);
+  const [currentMessageId, setCurrentMessageId] = useState("");
   const currentRealtor = useSelector(selectCurrentRealtor);
 
-  const { data, status, error } = useFetch(url);
+  const { data, status } = useFetch(url);
 
   const previousRealtor = usePrevious(currentRealtor);
   useEffect(() => {
     if (currentRealtor && currentRealtor.id && pageNo) {
-      if (previousRealtor !== currentRealtor) {
+      if (previousRealtor && previousRealtor.id !== currentRealtor.id) {
         setPageNo(1);
         setMessageList([]);
       }
@@ -46,20 +47,42 @@ export default function MessageList() {
     },
   });
 
+  const dispatch = useDispatch();
+  const handleClickLi = (message) => {
+    setCurrentMessageId(message.id);
+    if (!message.read) {
+      dispatch(setCurrentRealtor({
+        ...currentRealtor, unread_messages: currentRealtor.unread_messages - 1,
+      }));
+      const newMessageList = messageList.map((item) => {
+        if (item.id === message.id) {
+          fetch(`http://localhost:8080/realtors/${currentRealtor.id}/messages/${message.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ ...item, read: true }),
+          })
+            .then((response) => response.json());
+          return { ...item, read: true };
+        }
+        return item;
+      });
+      setMessageList(newMessageList);
+    }
+  };
+
   return (
     <ul className="MessageList">
-      {status}
-      {error}
       {
           messageList.length > 1 && messageList.map((message) => (
             <li key={message.id}>
-              <Link to={`/message/${message.id}`}>
+              <Link onClick={() => handleClickLi(message)} to={`/message/${message.id}`}>
                 <MessageListItem
                   subject={`${message.contact.firstname} ${message.contact.lastname}`}
                   description={message.body}
                   time={message.date}
                   icon={message.type}
                   read={message.read}
+                  active={currentMessageId === message.id}
                 />
               </Link>
             </li>
